2d4157b449f81ff69e9b84fdb27d502482dec731
Abdul Karim <karim.develops@gmail.com>
2025-09-22 20:59:38 +0500

    Log weather data of place searched by user

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./style.css";
 
-console.log("View this template at https://github.com/karimdevelops/webpack-template");
+const form = document.getElementById("form");
+const formInput = document.getElementById("formInput");
+
+form.addEventListener("submit", (e) => {
+    const placeName = formInput.value;
+    console.log(getWeather(placeName));
+    formInput.value = "";
+    e.preventDefault();
+})
+
+async function getWeather(placeName) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${placeName}?unitGroup=metric&key=K6ZZ56DKPXKJKQ2FR5VXBFPEP&contentType=json`);
+
+    const result = await response.json();
+    return result;
+}
