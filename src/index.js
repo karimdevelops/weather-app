2e1155de8d296890d59b0c6252aae00202709e60
Abdul Karim <karim.develops@gmail.com>
2026-03-16 17:58:24 +0500

    feat: display initial temp according to user city

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import "./style.css";
 import "./home"
 import { updateDisplay } from "./home";
 
-updateDisplay("Dubai");
+fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        getCity(data.ip);
+    })
+    .catch(error => console.error("Error fetching IP:", error));
+
+async function getCity(ip) {
+    fetch(`https://ipapi.co/${ip}/json/`)
+        .then(res => res.json())
+        .then(data => (updateDisplay(data.city)));
+}
 
 const form = document.getElementById("form");
 const formInput = document.getElementById("formInput");
